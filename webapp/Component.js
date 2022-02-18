ce11c8c41afc55750252fe2972341214f55a7047
Hleb Shynkarou <hleb_shynkarou@epam.com>
2022-02-18 16:40:28 +0300

    feat: move resource model to manifest

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,21 +1,12 @@
 sap.ui.define(
-  [
-    "sap/ui/core/UIComponent",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel",
-  ],
-  function (UIComponent, JSONModel, ResourceModel) {
+  ["sap/ui/core/UIComponent", "sap/ui/model/json/JSONModel"],
+  function (UIComponent, JSONModel) {
     "use strict";
 
     return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
       metadata: {
         interfaces: ["sap.ui.core.IAsyncContentCreation"],
-        rootView: {
-          viewName: "sap.ui.demo.walkthrough.view.App",
-          type: "XML",
-          /*"async": true, // implicitly set via the sap.ui.core.IAsyncContentCreation interface*/
-          id: "app",
-        },
+        manifest: "json",
       },
       /**
        * @override
@@ -32,15 +23,6 @@ sap.ui.define(
         const oModel = new JSONModel(oData);
 
         this.setModel(oModel);
-
-        // set i18n model on view
-        const i18nModel = new ResourceModel({
-          bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
-          supportedLocales: [""],
-          fallbackLocale: "",
-        });
-
-        this.setModel(i18nModel, "i18n");
       },
     });
   }
