c914c7ca569418523ca1f64d9bfd24285f234ae2
Hleb Shynkarou <hleb_shynkarou@epam.com>
2022-02-18 12:45:28 +0300

    feat: add json model

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,9 +1,24 @@
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
-  function (Controller, MessageToast) {
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel",
+  ],
+  function (Controller, MessageToast, JSONModel) {
     "use strict";
 
     return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+      onInit: function () {
+        const oData = {
+          recipient: {
+            name: "UI5",
+          },
+        };
+
+        const oModel = new JSONModel(oData);
+
+        this.getView().setModel(oModel);
+      },
       onShowHello: function () {
         MessageToast.show("Hello there!");
       },
