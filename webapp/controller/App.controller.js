863117e062b31527d7ed484ff61023013badfa1a
Hleb Shynkarou <hleb_shynkarou@epam.com>
2022-02-21 11:23:03 +0300

    feat: add nested view for HelloPanel

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,20 +1,5 @@
-sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
-  function (Controller, MessageToast) {
-    "use strict";
+sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
+  "use strict";
 
-    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-      onShowHello: function () {
-        // read msg from i18n model
-        const oBundle = this.getView().getModel("i18n").getResourceBundle();
-        const sRecipient = this.getView()
-          .getModel()
-          .getProperty("/recipient/name");
-        const sMsg = oBundle.getText("helloMsg", [sRecipient]);
-
-        // show msg
-        MessageToast.show(sMsg);
-      },
-    });
-  }
-);
+  return Controller.extend("sap.ui.demo.walkthrough.controller.App", {});
+});
