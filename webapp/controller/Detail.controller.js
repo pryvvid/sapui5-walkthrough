adcdefdda8067c6ab868e23db0850be95d03aa6a
Hleb Shynkarou <hleb_shynkarou@epam.com>
2022-02-23 11:49:39 +0300

    feat: add detail page back routing

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,25 +1,40 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
-  "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/ui/core/routing/History"],
+  function (Controller, History) {
+    "use strict";
 
-  return Controller.extend("sap.ui.demo.walkthrough.controller.Detail", {
-    onInit: function () {
-      const oRouter = this.getOwnerComponent().getRouter();
-      oRouter
-        .getRoute("detail")
-        .attachPatternMatched(this._onObjectMatched, this);
-    },
+    return Controller.extend("sap.ui.demo.walkthrough.controller.Detail", {
+      onInit: function () {
+        const oRouter = this.getOwnerComponent().getRouter();
+        oRouter
+          .getRoute("detail")
+          .attachPatternMatched(this._onObjectMatched, this);
+      },
 
-    _onObjectMatched: function (oEvent) {
-      console.log(oEvent.getParameters());
+      _onObjectMatched: function (oEvent) {
+        console.log(oEvent.getParameters());
 
-      this.getView().bindElement({
-        path:
-          "/" +
-          window.decodeURIComponent(
-            oEvent.getParameter("arguments").invoicePath
-          ),
-        model: "invoice",
-      });
-    },
-  });
-});
+        this.getView().bindElement({
+          path:
+            "/" +
+            window.decodeURIComponent(
+              oEvent.getParameter("arguments").invoicePath
+            ),
+          model: "invoice",
+        });
+      },
+
+      onNavBack: function () {
+        const oHistory = History.getInstance();
+        const sPreviousHash = oHistory.getPreviousHash();
+        console.log("sPreviousHash", sPreviousHash);
+        if (sPreviousHash !== undefined) {
+          window.history.go(-1);
+        } else {
+          const oRouter = this.getOwnerComponent().getRouter();
+          oRouter.navTo("overview", {}, true);
+        }
+      },
+    });
+  }
+);
