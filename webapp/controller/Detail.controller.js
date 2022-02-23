1c21c2d550412dd3e3ed2637212d79de892721ca
Hleb Shynkarou <hleb_shynkarou@epam.com>
2022-02-23 12:55:46 +0300

    feat: add custom controls

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,6 +1,10 @@
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/ui/core/routing/History"],
-  function (Controller, History) {
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/routing/History",
+    "sap/m/MessageToast",
+  ],
+  function (Controller, History, MessageToast) {
     "use strict";
 
     return Controller.extend("sap.ui.demo.walkthrough.controller.Detail", {
@@ -12,6 +16,8 @@ sap.ui.define(
       },
 
       _onObjectMatched: function (oEvent) {
+        this.byId("rating").reset();
+
         console.log(oEvent.getParameters());
 
         this.getView().bindElement({
@@ -34,6 +40,16 @@ sap.ui.define(
           const oRouter = this.getOwnerComponent().getRouter();
           oRouter.navTo("overview", {}, true);
         }
+      },
+      onRatingChange: function (oEvent) {
+        const fValue = oEvent.getParameter("value");
+        const oResourceBundle = this.getView()
+          .getModel("i18n")
+          .getResourceBundle();
+
+        MessageToast.show(
+          oResourceBundle.getText("ratingConfirmation", [fValue])
+        );
       },
     });
   }
