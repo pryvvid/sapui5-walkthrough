50584fa9b0cd1fd892c228ee24475f54da894971
Hleb Shynkarou <hleb_shynkarou@epam.com>
2022-02-23 16:42:13 +0300

    feat: add device adaptation

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -3,12 +3,18 @@ sap.ui.define(
     "sap/ui/core/mvc/Controller",
     "sap/ui/core/routing/History",
     "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel",
   ],
-  function (Controller, History, MessageToast) {
+  function (Controller, History, MessageToast, JSONModel) {
     "use strict";
 
     return Controller.extend("sap.ui.demo.walkthrough.controller.Detail", {
       onInit: function () {
+        const oViewModel = new JSONModel({
+          currency: "EUR",
+        });
+        this.getView().setModel(oViewModel, "view");
+
         const oRouter = this.getOwnerComponent().getRouter();
         oRouter
           .getRoute("detail")
